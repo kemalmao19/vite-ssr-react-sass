3e73784c3c25824a1c6c48f7c76ed565795adf13
kemalmao19 <sebelumtidur14@gmail.com>
2024-09-04 09:25:02 +0700

    chore: hero starter

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -2,7 +2,10 @@ import styles from "./hero.module.scss";
 export const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div className={styles.title}>This is home hero</div>
+      <div className={styles.title}>
+        <h1>Welcome to React-SaSS Starter</h1>
+        <a href="https://github.com/kemalmao19/vite-ssr-react-sass">Github</a>
+      </div>
     </div>
   );
 };
